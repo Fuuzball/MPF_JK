932e6125d1efe276d809fdaaa0bf575e7624b9fd
Michael Fang <1michaelfang@gmail.com>
2017-09-14 19:28:24 -0700

    fixed some factors and stuff

--- ising_sampler.py
+++ ising_sampler.py
@@ -265,6 +265,3 @@ if __name__ == '__main__':
     plt.show()
 
     
-
-
-
--- wolff.py
+++ wolff.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
+from mpf_ising_jk import MPF_Estimator
 np.random.seed(5)
 
 
@@ -9,7 +10,7 @@ def sampleX(J, D, N, burn_in, thin):
     Dy = D
 
     X = np.zeros((N, Dx, Dy))
-    x = np.random.randint(2, size=(Dx, Dy))
+    x = np.random.randint(2, size=(Dx, Dy)) * 2 - 1
 
     for i in range(n_sample_steps):
         d =  np.random.randint(D, size=(2))
@@ -35,7 +36,7 @@ def flip_cluster(X, d_init, J):
             for n in nn:
                 if ( X[n] == X[d] ) and ( n not in visited ) and (np.random.rand() < p):
                     new_to_flip.append(n)
-            X[d] = 1 - X[d]
+            X[d] *= -1
         visited.extend(to_flip)
         to_flip = set(new_to_flip)
 
@@ -71,15 +72,33 @@ def stackX(X, ratio = 1.5, pad = 1):
             
 
 if __name__ == '__main__':
-    N = 5
-    D = 20
+    N = 100
+    D = 50
 
-    burn_in = 100 * D**2
+    burn_in = 10 * D**2
+    burn_in = 2
     thin = 10 * D**2
+    thin = 2
 
-    Jstar = np.log(1 + np.sqrt(2)) / 2
-    J = Jstar
-    X = sampleX(J, D, 6, 100, 100)
-    X_stacked = stackX(X)
-    plt.imshow(X_stacked)
-    plt.show()
+    J_list = np.arange(0.75, 1.5, 0.05)
+    n_J = J_list.shape[0]
+    M_list = np.zeros_like(J_list)
+
+    x = np.random.randint(2, size=(D, D)) * 2 - 1
+    d =  np.random.randint(D, size=(2))
+    d = tuple(d)
+    flip_cluster(x, d, 0.2)
+
+
+
+
+    if True:
+        for i in range(n_J):
+            J = 1/J_list[i]
+            print(J)
+            X = sampleX(J, D, N, burn_in, thin)
+            M = X.mean(axis=(1,2))
+            M_list[i] = np.abs(M).mean()
+
+        plt.plot(J_list, M_list)
+        plt.show()
