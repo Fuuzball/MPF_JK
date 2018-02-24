fixed sign error

gibbs_torch.py
@@ -7,11 +7,14 @@ import time
 import matplotlib.pylab as plt
 import torch
 from torch.autograd import Variable
-from mpf_local_higher_order import MPF_Glass_HOLI, torch_double_var
 import logging
+import matplotlib.pylab as plt
+from holi_mpf_est import HOLIGlass
 
 logger = logging.getLogger(__name__)
 
+def torch_double_var(npy_arry, grad=False):
+    return Variable(torch.from_numpy(npy_arry).double(), requires_grad=grad)
 
 def stack_X(X, ratio = 1.5, W = None, pad = 1):
     N, Dx, Dy = X.shape
@@ -34,7 +37,7 @@ def stack_X(X, ratio = 1.5, W = None, pad = 1):
 
 class GibbsSampler(object):
 
-    def __init__(self, D, theta, EstClass, shape_2d=None):
+    def __init__(self, D, theta, estimator, shape_2d=None):
         self.logger = logger
         self.logger.info('Initializing GibbsSampler')
         self.theta = theta
@@ -44,7 +47,7 @@ class GibbsSampler(object):
             H = int(self.D / W)
             shape_2d = H, W
         self.shape_2d = shape_2d
-        self.EstClass = EstClass
+        self.estimator = estimator
 
     def get_rand_spins(self):
         return np.random.randint(2, size=self.D) * 2 - 1
@@ -58,17 +61,21 @@ class GibbsSampler(object):
         rand_p = np.random.random(n_sample_steps)
         X = np.zeros((N, self.D)) 
         x = self.get_rand_spins()
-        estimator = self.EstClass(x[None, :])
+        self.estimator.X = x[None, :]
 
         for n in range(n_sample_steps):
             i = rand_d[n]
-            dE = estimator.get_dE(self.theta)[0, i]
+            dE = self.estimator.get_dE(self.theta)[0, i]
             self.logger.debug('-'*20 + 'dE' + '-'*20 + '\n{}'.format(dE))
             p = exp(-dE)
             if p > rand_p[n]:
+                x_2d = (self.estimator.X_2d.data.numpy())
+                i_x = i // self.D**.5
+                i_y = i % self.D**.5
+                print(x_2d)
+                print(i_x, i_y, dE)
                 x[i] *= -1
-
-            estimator.X = torch_double_var(x[None, :], False)
+                self.estimator.X = x[None, :]
             
             if n >= burn_in and (n - burn_in) % thin == 0:
                 k = (n - burn_in) // thin
@@ -95,23 +102,31 @@ class GibbsSampler(object):
             plt.imsave(fname, X_stacked, format='png', cmap='Greys')
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%d-%m-%Y:%H:%M:%S',
+    level=logging.INFO)
+    logging.getLogger('torch_lbfgs.py').setLevel(logging.DEBUG)
+    D = 3**2
+    N = 1
 
-    D = 10
-    x = np.random.randint(2, size=(1, D**2)) * 2 - 1
+    rng = np.random.RandomState(seed=10)
 
-    J = np.random.normal(size=(D**2, D**2))
-    b = np.zeros(D**2)
-    K = np.random.normal(size=(D-1, D-1))
+    X = rng.randint(2, size=(N, D)) * 2 - 1
+    estimator = HOLIGlass(X, params=['b'], M=[])
+    params = estimator.get_random_params()
+    params['b'] = torch_double_var(
+            np.array(
+                [[100, -100, 100],
+                [-100, 100, 100],
+                [100, 100, -100]]
+                ).reshape(-1)
+            )
+    theta = estimator.flatten_params(params)
 
-    J = torch_double_var(J, False)
-    b = torch_double_var(b, False)
-    K = torch_double_var(K, False)
-
-    estimator = MPF_Glass_HOLI(x)
-    theta = (estimator.flatten_params(J, b, K))
-
-    sampler = GibbsSampler(D**2, theta, MPF_Glass_HOLI)
-    sampler.sample_X(10, 100, 500)
+    sampler = GibbsSampler(D, theta, estimator)
+    N = 1
+    burn_in = 100
+    thin = 100
+    X = sampler.sample_X(N, burn_in, thin)
     sampler.plot_sample()
-
+    plt.show()

holi_mpf_est.py
@@ -24,7 +24,7 @@ def get_rand_J(D):
     return J
 
 
-def torch_double_var(npy_arry, grad):
+def torch_double_var(npy_arry, grad=False):
     return Variable(torch.from_numpy(npy_arry).double(), requires_grad=grad)
 
 
@@ -42,7 +42,7 @@ def make_arr_torch(arr, arr_name, req_grad=False):
     elif is_Variable(arr):
         return arr.double()
     else:
-        logger.error(arr_name + 'needs to be either np array or torch variable')
+        logger.error(arr_name + ' needs to be either np array or torch variable')
         sys.exit()
 
 
@@ -51,6 +51,7 @@ class HOLIGlass(object):
     def __init__(self, X, shape_2d=None, M=None, params=['J_glass', 'b']):
         logger.info('Initializing HOLIGlass...')
         self.N, self.D = X.shape
+        self.corr_mats = OrderedDict()
 
         #Convert to float
         #self.X = Variable(torch.from_numpy(X).type(torch.DoubleTensor), requires_grad=False)
@@ -75,7 +76,6 @@ class HOLIGlass(object):
         self.M = M
 
         # Make 2nd order correlations for local ising model
-        self.corr_mats = OrderedDict()
         for name in params:
             if 'j_' in name:
                 k = int(name[2:])
@@ -123,6 +123,7 @@ class HOLIGlass(object):
     def X(self, new_X):
         self._X = make_arr_torch(new_X, 'X')
         self._X_2d =  self._X.view((self.N, self.H, self.W))
+        self.update_corr_mat()
 
     @property
     def X_2d(self):
@@ -187,6 +188,7 @@ class HOLIGlass(object):
 
         return params
             
+    ## NOT BEING USED ##
     def get_W(self, J):
         a = torch.Tensor([[4,1,0,1,4]])
         r2 = a + a.t()
@@ -203,10 +205,14 @@ class HOLIGlass(object):
         W = self.get_W(J)
         H = F.conv2d( self.X_2d[:, None, :, :], W[None, None, :, :], padding=2)
         H = H.squeeze(1) # Get rid of the channel dimension
-        return H
-        dE_2d = - 2 * self.X_2d * H
-        return dE_2d.view(self.N, -1)
+        return self.X_2d * H
 
+    def update_corr_mat(self):
+        for name in self.corr_mats:
+            k = int(name[2:])
+            self.corr_mats[name] = self.get_corr_mat(k)
+
+    ## NOT BEING USED ##
     def dE_local_ising(self, J):
         W = self.get_W(J)
         H = F.conv2d( self.X_2d[:, None, :, :], W[None, None, :, :], padding=2)
@@ -300,7 +306,7 @@ class HOLIGlass(object):
             if 'j_' in p:
                 C = self.corr_mats[p]
                 j = params[p]
-                dE += - 2 * j * self.X * C.view(self.N, -1)
+                dE += j * C.view(self.N, -1)
 
         # Energy due to higher order local interaction
         k_params = [params[p] for p in params if 'k_' in p]
@@ -383,20 +389,22 @@ if __name__ == '__main__':
     datefmt='%d-%m-%Y:%H:%M:%S',
     level=logging.INFO)
     logging.getLogger('torch_lbfgs.py').setLevel(logging.DEBUG)
-    D = 4**2
-    N = 100
+    D = 10**2
+    N = 1
 
-    X = np.random.randint(2, size=(N, D)) * 2 - 1
+    rng = np.random.RandomState(seed=10)
 
-    M1 = np.ones((2,2))
-    M2 = np.array(
-            [
-                [1, 0, 1],
-                [0, 1, 0],
-                [1, 0, 1]
-                ]
-            )
-
-    holi = HOLIGlass(X, params=['j_1', 'b'])
-    params = holi.learn(max_iter=1000 )
+    X = rng.randint(2, size=(N, D)) * 2 - 1
+    estimator = HOLIGlass(X, params=['j_1'], M=[])
+    params = estimator.get_random_params()
+    params['j_1'] = torch_double_var(np.array([20]))
     print(params)
+    theta = estimator.flatten_params(params)
+
+    sampler = GibbsSampler(D, theta, estimator)
+    N = 30
+    burn_in = 10
+    thin = 10
+    X = sampler.sample_X(N, burn_in, thin)
+    sampler.plot_sample()
+    plt.show()

mnist_fit.py
@@ -14,7 +14,7 @@ X = mnist.data
 
 N_X = X.shape[0]
 
-n_samples = 1000
+n_samples = 10000
 
 M0 = np.ones((2, 2))
 M1 = np.ones((2, 2))
@@ -36,17 +36,18 @@ else:
     X_sample = X
 print(X_sample.shape)
 
-estimator = HOLIGlass(X_sample, params=['j_1', 'b'], M=M)
+estimator = HOLIGlass(X_sample, params=['j_1', 'j_2', 'j_3', 'j_4', 'b'], M=M)
 
 def f(prop):
     print (prop['n_iter'], prop['abs_grad_sum'], prop['loss'])
 
 est_params_lr_5 = {
-        'lr' : 1,
+        'lr' : .1,
         'max_iter' : 50,
         'tolerance_grad' : 1,
         'tolerance_change' : 1E-5,
-        'call_back' : f
+        'call_back' : f,
+        'history_size' : 10
         }
 
 est_params = {
@@ -63,12 +64,16 @@ params = estimator.learn(params=est_params_list)
 #print(params)
 plt.subplot(221)
 plt.imshow(params['k_0'])
+plt.colorbar()
 plt.subplot(222)
 plt.imshow(params['k_1'])
+plt.colorbar()
 plt.subplot(223)
 plt.imshow(params['k_2'])
+plt.colorbar()
 plt.subplot(224)
 plt.imshow(params['k_3'])
+plt.colorbar()
 print(params)
 plt.show()
 #pickle.dump(params, open('params.p', 'wb'))