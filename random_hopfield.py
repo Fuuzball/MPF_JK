80e7b6710fa0aeb66c5a4ebcf572deca4e476ac8
Michael Fang <1michaelfang@gmail.com>
2018-02-27 17:27:27 -0800

    impliment better file organization

--- random_hopfield.py
+++ random_hopfield.py
@@ -7,31 +7,43 @@ import random
 import time
 import matplotlib.pylab as plt
 from sklearn.datasets import fetch_mldata
+import os
 
-rng = np.random.RandomState(0)
+rng = np.random.RandomState(1)
 
 D_list = np.arange(10, 110, 10)
 N_list = np.arange(10, 160, 10)
 frac_min_arr = np.zeros((len(D_list), len(N_list)))
 
-f_name = './frac_min_arr_mpf.npy'
-try:
-    frac_min_arr = np.load(f_name)
-except:
-    for d_i, D in enumerate(D_list):
-        for n_i, N in enumerate(N_list):
 
-            X = rng.randint(0, 2, size=(N, D)) * 2 - 1
+time_stamp = str(int(time.time()))
+dir_path = os.path.dirname(os.path.realpath(__file__))
+path = os.path.join(dir_path, 'data', 'random_capacity', str(time_stamp))
+os.mkdir(path)
 
-            model = HOLIGlass(X, M=[], params=['J_glass'])
-            theta = model.learn(False)
-            #theta = (X.T @ X).reshape(-1)
-            dE = model.get_dE(theta).data.numpy()
-            frac_min = (dE > 0).all(axis=1).sum()/N
-            frac_min_arr[d_i, n_i] = frac_min
-            print(D, N, frac_min)
+meta_data_path = os.path.join(path, 'meta_data')
+npy_arr_path = os.path.join(path, 'frac_min.npy')
+plot_path = os.path.join(path, 'min_frac_plot.png')
 
-np.save(f_name, frac_min_arr)
+with open(meta_data_path, 'w') as f:
+    f.write(f'D: {D_list}\n')
+    f.write(f'N: {N_list}')
+
+for d_i, D in enumerate(D_list):
+    for n_i, N in enumerate(N_list):
+        print(D, N)
+
+        X = rng.randint(0, 2, size=(N, D)) * 2 - 1
+
+        model = HOLIGlass(X, M=[], params=['J_glass'])
+        
+        #theta = model.learn(unflatten=False, params=[{'lr' : .5, 'max_iter' : 100}])
+        theta = (X.T @ X).reshape(-1)
+        dE = model.get_dE(theta).data.numpy()
+        frac_min = (dE > 0).all(axis=1).sum()/N
+        frac_min_arr[d_i, n_i] = frac_min
+
+np.save(npy_arr_path, frac_min_arr)
 
 plt.imshow(frac_min_arr.T, cmap='Greys_r', origin='lower')
-plt.show()
+plt.savefig(plot_path)
